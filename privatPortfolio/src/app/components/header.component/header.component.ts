7385c1061d9bbbe3e9fcf5d25750e6dade5969cb
hello90343 <deine-mail@>
2026-01-30 01:54:42 +0100

    header styling

--- privatPortfolio/src/app/components/header.component/header.component.ts
+++ privatPortfolio/src/app/components/header.component/header.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  currentLang: 'DE' | 'EN' = 'DE';
 
+  translatorSwitch(lang: 'DE' | 'EN') {
+    this.currentLang = lang;
+  }
 }
+
