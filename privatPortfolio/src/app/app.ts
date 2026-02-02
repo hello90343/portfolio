50959b4b8d2bd06fb781fa4e6cc1da21da5bc47f
hello90343 <deine-mail@>
2026-02-02 01:01:39 +0100

    about-section base of the section

--- privatPortfolio/src/app/app.ts
+++ privatPortfolio/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header.component/header.component';
 import { HeroComponent } from './components/hero.component/hero.component';
 import { SocialLinksSectionComponent } from './components/social-links-section.component/social-links-section.component';
+import { AboutSectionComponent } from './components/about-section.component/about-section.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, SocialLinksSectionComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, SocialLinksSectionComponent, AboutSectionComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
