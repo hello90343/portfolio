27cec8d370d1ee572389ca8e33a4056efc606199
hello90343 <deine-mail@>
2026-02-05 00:56:22 +0100

    added projects.component

--- privatPortfolio/src/app/app.ts
+++ privatPortfolio/src/app/app.ts
@@ -5,10 +5,11 @@ import { HeroComponent } from './components/hero.component/hero.component';
 import { SocialLinksSectionComponent } from './components/social-links-section.component/social-links-section.component';
 import { AboutSectionComponent } from './components/about-section.component/about-section.component';
 import { SkillsSectionComponent } from './components/skills-section.component/skills-section.component';
+import { ProjectsComponent } from './components/projects.component/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, SocialLinksSectionComponent, AboutSectionComponent, SkillsSectionComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, SocialLinksSectionComponent, AboutSectionComponent, SkillsSectionComponent, ProjectsComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
